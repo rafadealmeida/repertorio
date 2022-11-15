53d94770459a543388d8b22e557b9716d3b03aac
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-11-15 00:12:53 -0300

    Cifras individuais

--- src/pages/Teste.jsx
+++ src/pages/Teste.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Drawer from '../components/Drawer';
 
 function Teste() {
   return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
-      <Drawer />
       <h1>Teste</h1>
     </>
   );
