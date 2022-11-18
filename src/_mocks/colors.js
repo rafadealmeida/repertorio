86491ad5a1df148dd120e7e8312e4fac6aa354c3
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-11-17 22:53:00 -0300

    Resolvendo porblema de cores

---
+++ src/_mocks/colors.js
@@ -0,0 +1,32 @@
+const color = [
+  {
+    categoria: 'Maria',
+    color: '#83D5F1',
+  },
+  {
+    categoria: 'Paz',
+    color: '#DADDDB',
+  },
+  {
+    categoria: 'Decolagem',
+    color: '#EBA75F',
+  },
+  {
+    categoria: 'Louvor',
+    color: '#F77B72',
+  },
+  {
+    categoria: 'Espirito',
+    color: '#FFD966',
+  },
+  {
+    categoria: 'Perdao',
+    color: '#BADBE1',
+  },
+  {
+    categoria: 'Novas',
+    color: '#B4A7D6',
+  },
+];
+
+export default color;
