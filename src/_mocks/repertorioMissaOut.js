7e3209db35ad09ecb30ef81decc73f2e395d1c98
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2023-09-29 06:57:21 -0300

    [master] - Ajueste de repertório

--- src/_mocks/repertorioMissaOut.js
+++ src/_mocks/repertorioMissaOut.js
@@ -235,12 +235,92 @@ const repertorioMissaOut = [
     base: false,
     categoria: 'Salmo',
     cifra: `
-    Tom: C
-    Refrão:
-    C G/B Am   F Am G C4 C
-    Estrofe:
-    C G/B Am   F   Am G
+    INTRO: F, C, G, Am7, F, C, G
+
+      Am7         F       C         G
+    A VINHA DO SENHOR É A CASA DE ISRAEL
+
+            F          C           G    Am7
+    1. Arrancastes do Egito esta videira,
+            F           C             G
+    e expulsastes as nações para plantá-la;
+          F           C             G    Am7
+    até o mar se estenderam seus sarmentos,
+          F             C              G
+    até o rio os seus rebentos se espalharam.
+
+                  F            C          G    Am7
+    2. Por que razão vós destruístes sua cerca,
+              F           C           G
+    para que todos os passantes a vindimem,
+          F          C          G    Am7
+    o javali da mata virgem a devaste,
+            F             C          G
+    e os animais do descampado nela pastem?
+
+          F            C               G    Am7
+    3. Voltai-vos para nós, Deus do universo!
+      F             C           G
+    Olhai dos altos céus e observai.
+        F           C            G    Am7
+    Visitai a vossa vinha e protegei-a!
+          F           C               G
+    Foi a vossa mão direita que a plantou;
+          F             Am7          G
+    protegei-a, e ao rebento que firmastes!
+
+                F             C             G    Am7
+    4. E nunca mais vos deixaremos, Senhor Deus!
+            F            C           G
+    Dai-nos vida, e louvaremos vosso nome!
+          F             C              G    Am7
+    Convertei-nos, ó Senhor Deus do universo,
+            F        C           G
+    e sobre nós iluminai a vossa face!
+          F           Am7             G
+    Se voltardes para nós, seremos salvos!
     `,
+  },
+  {
+    titulo: 'Salmo 24',
+    id: sumId(),
+    link: 'https://www.youtube.com/watch?v=wqBuVL8alAA',
+    base: false,
+    categoria: 'Salmo',
+    cifra: `
+
+    D A Bm G
+
+    RECORDAI, SENHOR MEU DEUS,
+    VOSSA TERNURA E COMPAIXÃO!(BIS)
+
+    MOSTRAI-ME, Ó SENHOR, VOSSOS CAMINHOS,
+    E FAZEI-ME CONHECER A VOSSA ESTRADA!
+    VOSSA VERDADE ME ORIENTE E ME CONDUZA,
+    PORQUE SOIS O DEUS DA MINHA SALVAÇÃO;
+
+    EM VÓS ESPERO, Ó SENHOR, TODOS OS DIAS! (BIS)
+
+    RECORDAI, SENHOR MEU DEUS,
+    VOSSA TERNURA E COMPAIXÃO!
+
+    RECORDAI, SENHOR MEU DEUS, VOSSA TERNURA
+    E A VOSSA COMPAIXÃO QUE SÃO ETERNAS!
+    NÃO RECORDEIS OS MEUS PECADOS QUANDO JOVEM,
+    NEM VOS LEMBREIS DE MINHAS FALTAS E DELITOS!
+    DE MIM LEMBRAI-VOS, PORQUE SOIS MISERICÓRDIA
+    E SOIS BONDADE SEM LIMITES, Ó SENHOR!
+
+    RECORDAI, SENHOR MEU DEUS,
+    VOSSA TERNURA E COMPAIXÃO!
+
+    O SENHOR É PIEDADE E RETIDÃO,
+    E RECONDUZ AO BOM CAMINHO OS PECADORES.
+    ELE DIRIGE OS HUMILDES NA JUSTIÇA,
+    E AOS POBRES ELE ENSINA O SEU CAMINHO.
+
+    RECORDAI, SENHOR MEU DEUS,
+    VOSSA TERNURA E COMPAIXÃO!(BIS)`,
   },
   {
     titulo: 'Quando estámos unidos',
@@ -587,6 +667,332 @@ const repertorioMissaOut = [
     Onde Cristo é tudo em todos, vem à vida nova se lançar
     C      D7           G
       (Alçando vôo se lançar)
+    `,
+  },
+  {
+    titulo: 'Espírito da Verdade',
+    id: sumId(),
+    categoria: 'Novas',
+    cifra: `
+    [Intro] C  C9  F  C  C9  F
+
+    C              C9
+    Espírito da verdade
+    C              C9
+    Espírito da verdade
+        Am
+    Ouça a minha voz
+
+        F                     G
+    Já não quero o que não te agrada
+        F                G
+    Renuncio o que nos afasta
+
+    C              Am
+    Sopra dos quatro cantos
+            F
+    Espírito Santo
+                C              G
+    E enche meu coração, meu coração
+
+    `,
+  },
+  {
+    titulo: 'JESUS FILHO DE DAVI',
+    id: sumId(),
+    categoria: 'Novas',
+    cifra: `
+    [Intro] C  F  C  F
+
+        C                        G
+    Me contaram que o Senhor ia passar
+            Dm                      G
+    E que havia uma chance de eu poder enxergar
+        C                   G
+    Me disseram que tinhas poder
+        Dm                       G
+    Pra ressuscitar e fazer o paralítico andar
+
+    Am                     Em    F
+    Renasceu dentro de mim a fé
+                        G
+    Que há muito eu já não tinha
+    Am                   Em    F
+    Eu senti meu coração arder
+                        G
+    E eu gritei para o Senhor me responder:
+
+    C                    G     Am
+    Jesus, filho de Davi, me cura
+        F           C
+    És o Santo de Israel
+                        G       Am
+    Diante de Ti a tempestade se cala
+            G             C
+    Meu Deus honra a minha fé  (2x)
+    `,
+  },
+  {
+    titulo: 'TE LOUVO EM VERDADE',
+    id: sumId(),
+    categoria: 'Novas',
+    cifra: `
+    [Intro]  E  F#m  G#m  A  E  B9  E  B9
+
+    E              F#m   G#m                  A9
+   Mesmo na tempestade, mesmo que se agite o mar
+    E            B9          E    B9
+   Te louvo, te louvo em verdade
+
+    E               F#m      G#m       A9
+   Mesmo longe dos meus, mesmo na solidão
+    E            B9          E   E7
+   Te louvo, te louvo em verdade
+
+           A9            B9    G#m           C#m
+   Pois somente tenho a Ti, Tu és a minha herança
+    A9           B9          E   E7
+   Te louvo, te louvo em verdade
+
+           A9            B9    G#m           C#m
+   Pois somente tenho a Ti, Tu és a minha herança
+    A9           B9
+   Te louvo, te louvo em verdade
+
+   E  F#m  G#m  A  E  B9  E  B9
+
+
+   Mesmo que me faltem palavras
+   Mesmo que eu não saiba louvar
+   Te louvo, te louvo em verdade
+   Mesmo que me faltem palavras
+   Ainda que eu não saiba louvar
+   Te louvo, te louvo em verdade
+    `,
+  },
+  {
+    titulo: 'Incendeia Minha Alma',
+    id: sumId(),
+    categoria: 'Novas',
+    cifra: `
+    [Intro] C  Am  Dm7  G4
+    C  Am  Dm7  G  G4
+
+    [Primeira Parte]
+
+    C           G/B         Am
+    Espírito Santo, vinde
+            G
+    Falar em mim
+    C           G/B         Am
+    Espírito Santo, vinde
+            G
+    Falar em mim (2X)
+
+    [Pré-Refrão]
+
+        C/E  F               Am
+    Vinde curar, vinde libertar
+                Dm      C/E  G
+    Nossos corações de toda  opressão
+        C/E        F              Am
+    Vinde transformar, vem incendiar
+                D           G
+    Traz fogo do céu nesse lugar
+
+    [Refrão]
+
+        C              G/B
+    Incendeia minha alma
+        Am             Am/G
+    Incendeia minha alma
+        F         C/E       G4  G
+    Incendeia minha alma, Senhor    (2x)
+
+    [Primeira Parte]
+
+    [Pré-Refrão]
+
+    [Refrão]
+
+    `,
+  },
+  {
+    titulo: 'Posso Tudo Posso',
+    id: sumId(),
+    categoria: 'Novas',
+    cifra: `
+    Tom: C
+
+    (intro) Em D C9 Am G D
+            C G/B Am D C G/B Am Am/G D
+
+       G       D/F#     C               Em    D
+    Posso, tudo posso Naquele que me fortalece
+     C               G/B                Am   D
+    Nada e ninguém no mundo vai me fazer desistir
+     G        D/F#        C                   Em      D
+    Quero, tudo quero, sem medo entregar meus projetos
+        C                 G/B                Am     D/F# C/E  D
+    Deixar-me guiar nos caminhos que Deus desejou pra mim e a.....li estar
+
+    [Refrão]
+    G          D/F#                  C/E   D
+    Vou perseguir tudo aquilo que Deus já escolheu pra mim
+              G     D/F#                 C
+    Vou persistir, e mesmo nas marcas daquela dor
+       G/B                   Am Dsus4 D
+    Do que ficou, vou me lembrar
+            G    D/F#                  C/E       D
+    E realizar o sonho mais lindo que Deus sonhou
+     Eb°     Em              Bm                  C
+    Em meu lugar estar na espera de um novo que vai chegar
+        G/B            Am    Am/G  D/F#    C/E     D
+    Vou persistir, continuar a esperar      e     crer
+
+     G/B    C           G/B      Am          D          C
+    E      mesmo quando a visão se turva e o coração só chora
+          G/B           Am         D
+    Mas na alma, há certeza da vitória
+
+
+
+       G       D/F#     C               Em    D
+    Posso, tudo posso Naquele que me fortalece
+     C               G/B                Am   D
+    Nada e ninguém no mundo vai me fazer desistir
+
+    [Repete Refrão]
+
+    C7M(9)        G/B           Am9          D
+    Eu vou sofrendo, mas seguindo enquanto tantos não entendem
+            C7M(9)       G/B            Am9
+    Vou cantando minha história, profetizando
+            D          C     G/B    Am9       D     G9
+    Que eu posso, tudo posso...                 em Jesus
+
+    `,
+  },
+  {
+    titulo: 'SÓ POR TI JESUS',
+    id: sumId(),
+    categoria: 'Novas',
+    cifra: `
+    [Intro]       Emaj7  F#m/E  Emaj7  F#m/E  B/C#  C#m  B  C#m Amaj7 F#m7 Amaj7/B
+
+    E7+   F#m/E             B/C#   C#m
+    Só por Ti, Jesus, quero me consumir,
+                A7+              F#m7                A/B
+    Como vela que queima no altar, me consumir de amor.
+      E7+      F#m/E             B/C#  C#m
+    Só em Ti, Jesus, quero me derramar,
+                    A7+      A/B             Emaj7 F#m/E
+    Como o rio se entrega ao mar, me derramar de amor.
+        E7+   F#m/E             B/C#   C#m
+    Só por Ti, Jesus, quero me consumir,
+                A7+              F#m7                A/B
+    Como vela que queima no altar, me consumir de amor.
+      E7+      F#m/E             B/C#  C#m
+    Só em Ti, Jesus, quero me derramar,
+                    A7+      A/B             Emaj7 F#m/E
+    Como o rio se entrega ao mar, me derramar de amor.
+
+      A7+               B/A
+    Pois Tu és o meu amparo, o meu refúgio,
+                      C#m (B/C# C#m)
+    és alegria de minh'alma.
+    A7+               B/A        G#m7
+    Só em Ti repousa a minha esperança, não vacilarei,
+            C#m7          F#m7       D A/C#  A/B
+    e mesmo na dor, quero seguir até o fim.
+      E7+   E6                E7+  E6
+        E7+   F#m/E             B/C#   C#m
+    Só por Ti, Jesus, quero me consumir,
+                A7+              F#m7                A/B
+    Como vela que queima no altar, me consumir de amor.
+
+    `,
+  },
+  {
+    titulo: 'TÃO SUBLIME SACRAMENTO',
+    id: sumId(),
+    categoria: 'Novas',
+    cifra: `
+    D                  G        D      BM   EM      G  A  D
+    Tão sublime sacramento, adoremos neste altar
+    D                  G        D      BM      EM      G  A  D
+    Pois a antigo testamento deu ao novo seu lugar
+     G           D  G              D     BM   EM        G A  D
+    Venha a fé por suplemento os sentidos completai
+    D                  G        D      BM   EM      G  A  D
+    Ao eterno Pai cantemos  e a Jesus o salvador
+    D                  G        D  BM    EM        G      A  D
+    Ao Espírito exaltemos na Trindade eterno amor
+    G           D       G         D     BM   EM   G   A   D    G D A D
+    Ao Deus uno, e trino demos a alegria do louvor, Amém
+
+    `,
+  },
+  {
+    titulo: 'EM TUA PRESENÇA',
+    id: sumId(),
+    categoria: 'Novas',
+    cifra: `
+    G                      Am
+    É teu este momento de adoração
+     Bm7                         Am
+    Não tenho nem palavras para me expressar
+     G                                Am
+    No brilho dessa luz que vem do teu olhar
+       C          D           G
+    Encontro meu abrigo meu lugar
+
+      G                           Am
+    E quando estamos juntos entre nós estás
+     Bm7                         Am
+    Passando em nosso meio a nos abençoar
+     G                                Am
+    E tocas com ternura com a tua mão
+     C             D           G
+    A cada um que  abre o coração
+
+       G         Em7   Em/D    Am   Am/G         D D/C
+    Minhas mãos  se elevam    minha     voz te louva
+       Bm    E4  E7         Am  Am/G       D     G           D  D7
+    O meu ser se alegra/quando estou  em tua presença Senhor
+
+    `,
+  },
+  {
+    titulo: 'ALFA, OMEGA, PRINCÍPIO E FIM',
+    id: sumId(),
+    categoria: 'Novas',
+    cifra: `
+    E       G#M              A7+ C#M         A7+E/G#     F#M B7
+    Alfa,Ômega, Princípio e Fim, sim Ele É,   sim Ele É.
+    E       G#M              A7+ C#M         A7+E/G#     F#M B7
+    Alfa,Ômega, Princípio e Fim, sim Ele É,   sim Ele É.
+    E B/D#  C#M           E/G# A7+                  F#M7       A/B                 E
+    Lírio do Vale, Estrela     da Manhã, para sempre cantarei o seu louvor.
+    E  B/D#  C#M           E/G# A7+  B7                 E           B/D#         C#M
+    Lírio do Vale, Estrela     da Manhã, A Ele a Glória, a Ele o Louvor,
+                    A7+                  D   B7
+    A Ele a Domínio, Ele é o Senhor.
+                   E              B/D#   C#M           A7+         B7
+    A Ele a Glória, a Ele o Louvor, a Ele o Domínio, Ele é ...
+    E       G#M              A7+ C#M         A7+E/G#     F#M B7
+    Alfa,Ômega, Princípio e Fim, sim Ele É,   sim Ele É.
+    E       G#M              A7+ C#M         A7+E/G#     F#M B7
+    Alfa,Ômega, Princípio e Fim, sim Ele É,   sim Ele É.
+    E B/D#  C#M                E/G# A7+              F#M7       A/B                 E
+    Lírio do Vale, Estrela     da Manhã, para sempre cantarei o seu louvor.
+    E  B/D#  C#M           E/G# A7+  B7  E           B/D#         C#M
+    Lírio do Vale, Estrela     da Manhã, a Ele a Glória, a Ele o Louvor,
+                    A7+                  D   B7
+    A Ele a Domínio, Ele é o Senhor.
+    E              B/D#   C#M           A7+         B7
+    A Ele a Glória, a Ele o Louvor, a Ele o Domínio, Ele é ...    (várias vezes)
+
     `,
   },
 ];
